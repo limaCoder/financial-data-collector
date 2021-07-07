2e1aee20bcd00ce67ae81249342d18f88a5dbf66
Mario Lima <marioaulima15@outlook.com>
2021-07-07 10:25:36 -0300

    FEAT: index.html finished

--- src/api/services/cdi_rate.js
+++ src/api/services/cdi_rate.js
@@ -20,6 +20,7 @@ export async function RoboTaxaCDI(req, res) {
     }
 
     return res.status(200).json({status: true, response: [{cdi: resultado, message: `A Taxa CDI esse mês está valendo: ${resultado}`}]});
+    
   } catch(err) {
     console.log(err.message);
     return res.status(400).json({status: false, response: [], log: err.message});
